92665ccae9feff008f926f440037f0171d0493ab
Ben Wolsieffer <benwolsieffer@gmail.com>
2017-07-25 13:47:51 -0400

    Add data utils documentation.

--- depth_network/data_utils.py
+++ depth_network/data_utils.py
@@ -9,7 +9,26 @@ _logger = logging.getLogger(__name__)
 
 
 class HDFGenerator:
+    """
+    Generator that loads data from an HDF5 file and normalizes it.
+    """
+
     def __init__(self, x_dataset, y_dataset, batch_size=16, shuffle=False, normalizer=None):
+        """
+        Create an HDFGenerator.
+
+        Due to the low performance of the HDF5 library with random access, shuffling causes each batch to consist of a
+        contiguous block of samples (taken from a random index place within the dataset), which are then shuffled,
+        rather than individual samples chosen randomly from the dataset.
+
+        After :func:`HDFGenerator.steps_per_epoch` iterations, all the data in the dataset is guaranteed to be returned.
+
+        :param x_dataset: name of the x (input) dataset
+        :param y_dataset: name of the y (output) dataset
+        :param batch_size: number of samples to yield at once
+        :param shuffle: if true, shuffle the data
+        :param normalizer: normalizer function, which processes entire batches (both x and y) at once
+        """
         self.x_dataset = x_dataset
         self.y_dataset = y_dataset
         self.cur_index = (len(x_dataset) // batch_size) - 3
@@ -80,12 +99,23 @@ def _u(string):
 
 
 def merge_data_files(file_path, source_files, dataset_name):
+    """
+    Merge multiple HDF5 files into one virtual dataset. This virtual dataset appears as one large dataset when used with
+    the normal HDF5 APIs, but internally it is referencing each merged file. Virtual datasets are only supported in
+    HDF5>=1.10.
+
+    :param file_path: output file path
+    :param source_files: list of source files
+    :param dataset_name: name of the dataset to merge
+    """
     _logger.info("Creating virtual dataset file: %s", file_path)
 
     files = list(map(lambda f: h5f.open(_u(f), flags=h5f.ACC_RDONLY), source_files))
     datasets = []
     dataspaces = []
     num_elems = 0
+
+    # Virtual datasets are not yet supported in the high-level h5py API, so we have to use the low-level API
 
     for f in files:
         dataset = h5d.open(f, _u(dataset_name))
@@ -119,6 +149,17 @@ def merge_data_files(file_path, source_files, dataset_name):
 
 
 def fold_data(file_path, train_file_path, validation_file_path, dataset_name, validation_fold=0, folds=5):
+    """
+    Folds a dataset into training and validation sets. The training and validation datasets are virtual, so they do not
+    contain a copy of the data. This function can be used for performing k-fold cross validation.
+
+    :param file_path: path to full dataset
+    :param train_file_path: output training dataset file
+    :param validation_file_path: output validation dataset file
+    :param dataset_name: name of the dataset to fold
+    :param validation_fold: where to fold the data, 0<=validation_fold<folds
+    :param folds: number of folds
+    """
     file = h5f.open(_u(file_path))
     dataset = h5d.open(file, _u(dataset_name))
     dataspace = dataset.get_space()
